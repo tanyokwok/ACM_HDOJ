06c6dcf1e4bca60fc1cc9f15d1aa62ddc2c8e5ff
HouJP <peng_come_on@126.com>
2014-08-07 18:43:58 +0800

    this is a test

---
+++ test.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: test.cpp
+    > Author: HouJP
+    > Mail: peng_come_on@126.com 
+    > Created Time: 四  8/ 7 18:17:57 2014
+ ************************************************************************/
+
+#include <cstdio>
+#include <climits>
+
+int main() {
+	printf("%lf\n", DOUBLE_MAX);
+	return 0;
+}
